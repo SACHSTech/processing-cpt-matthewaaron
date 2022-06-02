d618e99c16d2465b9d2e76836a581b9f7830f182
Aaron-Chung23 <Aaron.Chung23@ycdsbk12.ca>
2022-06-02 08:50:27 -0400

    Commit

--- Sketch2.java
+++ Sketch2.java
@@ -1,14 +1,46 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch2 extends PApplet {
 	
-	
+  int row = 0;
+  int column = 0;
+
+  int rowCount = 3;
+  int colCount = 3;
+
+  int boxWidth = 200;
+  int boxHeight = 200;
+
+  int backgroundCount = 0;
+
+  boolean blueTrue = false;
+  boolean greenTrue = false;
+
+
+  boolean exWin = false;
+  boolean ohWin = false;
+  int exWincounter = 0;
+  int ohWincounter = 0;
+  String exWinStatement = "X Wins: ";
+  String ohWinStatement = "O Wins: ";
+
+  PImage uwuBg;
+  PImage pain;
+  PImage cart;
+  PImage OImage;
+  PImage XImage;
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
+    uwuBg = loadImage("uwu.png");
+    pain = loadImage("pain.png");
+    cart = loadImage("cart.png");
+    OImage = loadImage("O.png");
+    XImage = loadImage("x.png");
   }
 
   /** 
@@ -16,20 +48,73 @@ public class Sketch2 extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    //background(210, 255, 173);
   }
 
+  public void mousePressed() {
+    fill(0, 255, 0);
+    ellipse(mouseX, mouseY, 50, 50);
+  }
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
 	  
 	// sample code, delete this stuff
-     
-    textSize(144);
-    text(key, 110, 180);
-    
+  if(backgroundCount == 0){
+    image(pain, 0, 0);
   }
+  if(backgroundCount == 1){
+    image(cart, 0, 0);
+  }
+  if(backgroundCount == 2){
+    image(uwuBg,0,0);
+  }
+for(row = 0; row < rowCount; row++){
+  for(column = 0; column < colCount; column++){
+
+    int boxX = (boxWidth * row);
+    int boxY = (boxHeight * column);
+
+    stroke(255,0,0);
+    strokeWeight(10);
+    rect(boxX,boxY,boxWidth,boxHeight);
+  }
+}
+if (mousePressed){
+  greenTrue = true;
+
+
+  if greenTrue = true{
+    fill(23, 45, 34);
+  }
+  
+
+}
+  
+  }
+
+  public void mouseClicked(){
+        textSize(144);
+        text(key , 190, 180);
+     }
+
+  
+  public void keyPressed(){
+
+    if (key == 'b'){
+
+      backgroundCount++;
+      System.out.println(backgroundCount);
+      if(backgroundCount>=3){
+        backgroundCount = 0;
+      }
+
+    }
+
+
+  }
+
   
   // define other methods down here.
 }
