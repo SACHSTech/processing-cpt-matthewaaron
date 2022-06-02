7ee58ffa9d89ddcd0cc54c95a3f6ae8cf61ad7e0
Aaron-Chung23 <Aaron.Chung23@ycdsbk12.ca>
2022-06-02 09:25:57 -0400

    fff

--- Sketch2.java
+++ Sketch2.java
@@ -3,7 +3,7 @@ import processing.core.PImage;
 
 public class Sketch2 extends PApplet {
 	
-  int row = 0;
+	int row = 0;
   int column = 0;
 
   int rowCount = 3;
@@ -13,90 +13,103 @@ public class Sketch2 extends PApplet {
   int boxHeight = 200;
 
   int backgroundCount = 0;
-
-  boolean blueTrue = false;
-  boolean greenTrue = false;
-
+  int boxCounter  = 0;
 
   boolean exWin = false;
   boolean ohWin = false;
-  int exWincounter = 0;
-  int ohWincounter = 0;
-  String exWinStatement = "X Wins: ";
-  String ohWinStatement = "O Wins: ";
+  int blooWincounter = 0;
+  int greenWincounter = 0;
+  String blooWinStatement = "Blue Wins: ";
+  String greenWinStatement = "Green Wins: ";
 
   PImage uwuBg;
   PImage pain;
   PImage cart;
-  PImage OImage;
-  PImage XImage;
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
+  int [][] intGrid; 
+
+
   public void settings() {
-	// put your size call here
     size(800, 800);
     uwuBg = loadImage("uwu.png");
     pain = loadImage("pain.png");
     cart = loadImage("cart.png");
-    OImage = loadImage("O.png");
-    XImage = loadImage("x.png");
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+
   public void setup() {
     //background(210, 255, 173);
+    intGrid = new int[rowCount][colCount];
+    for(int x = 0; x < rowCount; x++){
+      for(int y = 0; y < colCount; y++){
+        intGrid[x][y] = 2;
+      }
+    }
+    
   }
 
-  public void mousePressed() {
-    fill(0, 255, 0);
-    ellipse(mouseX, mouseY, 50, 50);
-  }
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+
   public void draw() {
-	  
-	// sample code, delete this stuff
-  if(backgroundCount == 0){
-    image(pain, 0, 0);
-  }
-  if(backgroundCount == 1){
-    image(cart, 0, 0);
-  }
-  if(backgroundCount == 2){
-    image(uwuBg,0,0);
-  }
-for(row = 0; row < rowCount; row++){
-  for(column = 0; column < colCount; column++){
+    
+    if(backgroundCount == 0){
+      image(pain, 0, 0);
+    }
+    if(backgroundCount == 1){
+      image(cart, 0, 0);
+    }
+    if(backgroundCount == 2){
+      image(uwuBg,0,0);
+    }
+    winCounter();
+  for(row = 0; row < rowCount; row++){
+    for(column = 0; column < colCount; column++){
 
-    int boxX = (boxWidth * row);
-    int boxY = (boxHeight * column);
+      int boxX = (boxWidth * row);
+      int boxY = (boxHeight * column);
 
-    stroke(255,0,0);
-    strokeWeight(10);
-    rect(boxX,boxY,boxWidth,boxHeight);
+      stroke(255,0,0);
+      strokeWeight(10);
+      
+
+      if(intGrid[column][row] == 2){
+        fill(0,0,255);
+        rect(boxX,boxY,boxWidth,boxHeight);
+      }
+      if(intGrid[column][row] == 1){
+        fill(0,255,0);
+        rect(boxX,boxY,boxWidth,boxHeight);
+      }
+      if(intGrid[column][row] == 0){
+        fill(255,255,0);
+        rect(boxX,boxY,boxWidth,boxHeight);
+      }
+    
+    }
+    
+  }
+ 
+  }
+  public void mousePressed(){
+    if (mouseButton == LEFT) {
+      if(intGrid [mouseX][mouseY] == 1){
+
+        intGrid [mouseX][mouseY] = 0;
+     
+    } else if (mouseButton == RIGHT) {
+      if(intGrid [mouseX][mouseY] == 1){
+
+        intGrid [mouseX][mouseY] = 2;
+        
+    } else {
+      if(intGrid [mouseX][mouseY] == 0){
+  
+        intGrid [mouseX][mouseY] = 1;
+    
+    }
   }
 }
-if (mousePressed){
-  greenTrue = true;
-
-
-    fill(23, 45, 34);
-
-  
-
-}
-  
+    }
   }
-
-  public void mouseClicked(){
-        textSize(144);
-        text(key , 190, 180);
-     }
 
   
   public void keyPressed(){
@@ -114,6 +127,21 @@ if (mousePressed){
 
   }
 
-  
-  // define other methods down here.
+  public void winCounter(){
+
+
+    textSize(50);
+    fill(255,0,0);
+    text(blooWinStatement,0,700);
+    if(exWin = true){
+
+    }
+    textSize(50);
+    fill(255,0,0);
+    text(greenWinStatement,0,750);
+    if(ohWin = true){
+
+    }
+
+  }
 }
