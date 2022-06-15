10a9055f00206bd20c87fab495def494e5a6c440
Aaron-Chung23 <Aaron.Chung23@ycdsbk12.ca>
2022-06-14 23:53:44 -0400

    bob

--- Sketch2.java
+++ Sketch2.java
@@ -1,7 +1,7 @@
 import processing.core.PApplet;
 import processing.core.PImage;
 
-public class Sketch2 extends PApplet {
+public class Sketch extends PApplet {
 	
 	int row = 0;
   int column = 0;
@@ -33,6 +33,8 @@ public class Sketch2 extends PApplet {
   boolean turn = false;
   String clearText = "Space to Clear";
   boolean isFull = false;
+  String tie ="Tie （●｀ε´●）";
+  boolean winnerPlus = false;
 
   PImage uwuBg;
   PImage pain;
@@ -40,7 +42,9 @@ public class Sketch2 extends PApplet {
 
   int [][] intGrid;
 
-
+  /**
+  * Called once at the beginning of execution, put your size all/ loading images in this method
+  */
   public void settings() {
     size(800, 800);
     uwuBg = loadImage("uwu.png");
@@ -48,10 +52,11 @@ public class Sketch2 extends PApplet {
     cart = loadImage("cart.png");
   }
 
-
+/**
+  * Called once at the beginning of execution.  Add initial set up
+  * values here i.e background, stroke, fill etc.
+  */
   public void setup() {
-    //background(210, 255, 173);
-
     intGrid = new int[rowCount][colCount];
     for(int x = 0; x < rowCount; x++){
       for(int y = 0; y < colCount; y++){
@@ -61,17 +66,19 @@ public class Sketch2 extends PApplet {
     }
   }
 }
-
+/**
+  * Called repeatedly, anything drawn to the screen goes here
+  */
 
   public void draw() {
 
-    if(backgroundCount == 0){
+    if(backgroundCount == 2){
       image(pain, 0, 0);
     }
     if(backgroundCount == 1){
       image(cart, 0, 0);
     }
-    if(backgroundCount == 2){
+    if(backgroundCount == 0){
       image(uwuBg,0,0);
     }
     winCounter();
@@ -118,7 +125,9 @@ public class Sketch2 extends PApplet {
       }
       
   }
-  
+  /**
+   * Detects if a key is pressed and trigger the code that the key is assinged to
+   */
   public void keyPressed(){
 
     if (key == 'b'){
@@ -130,7 +139,8 @@ public class Sketch2 extends PApplet {
 
     }
 
-  //  if (key == 'l'){
+  //  Developer Hotkey
+  //if (key == 'l'){
 
     //  win = true;
    //   blooWin = true;
@@ -153,6 +163,7 @@ public class Sketch2 extends PApplet {
       blooWin = false;
       greenWin = false;
       gridFull = 0;
+      winnerPlus = false;
       
       }
       if (win == false && gridFull >= 9){
@@ -170,6 +181,7 @@ public class Sketch2 extends PApplet {
         blooWin = false;
         greenWin = false;
         gridFull = 0;
+        winnerPlus = false;
         
         
         }
@@ -179,111 +191,219 @@ public class Sketch2 extends PApplet {
 
     
   }
+  /**
+   * Check if green has 3 in a column and sets win condition
+   */
   public void greenCols(){
-    if (intGrid[0][0] == 1 && intGrid[0][1] == 1 && intGrid[0][2] == 1) {
+    if (intGrid[0][0] == 1 && intGrid[0][1] == 1 && intGrid[0][2] == 1 && winnerPlus == false) {
       win = true;
       greenWin = true;
+      winnerPlus = true;
+
+      if(winnerPlus == true){
+      greenWincounter+=1;
+      gridFull = 9;
+      }
   }
-  if (intGrid[1][0] == 1 && intGrid [1][1] == 1 && intGrid[1][2] == 1){
+  if (intGrid[1][0] == 1 && intGrid [1][1] == 1 && intGrid[1][2] == 1 && winnerPlus == false){
     win = true;
     greenWin = true;
+    winnerPlus = true;
+
+    if(winnerPlus == true){
+    greenWincounter+=1;
+    gridFull = 9;
+    }
   }
-  if (intGrid[2][0] == 1 && intGrid [2][1] == 1 && intGrid[2][2] == 1){
+  if (intGrid[2][0] == 1 && intGrid [2][1] == 1 && intGrid[2][2] == 1 && winnerPlus == false){
     win = true;
     greenWin = true;
+    winnerPlus = true;
+
+      if(winnerPlus == true){
+      greenWincounter+=1;
+      gridFull = 9;
+      }
   }
 
   }
+/**
+ * Checks if green has 3 in a row and sets win condition
+ */
   public void greenRows(){
-    if (intGrid[0][0] == 1 && intGrid[1][0] == 1 && intGrid[2][0] == 1) {
+    if (intGrid[0][0] == 1 && intGrid[1][0] == 1 && intGrid[2][0] == 1 && winnerPlus == false) {
       win = true;
       greenWin = true; 
+      winnerPlus = true;
+
+      if(winnerPlus == true){
+      greenWincounter+=1;
+      gridFull = 9;
+      }
     }
-    if (intGrid[0][1] == 1 && intGrid [1][1] == 1 && intGrid[2][1] == 1){
+    if (intGrid[0][1] == 1 && intGrid [1][1] == 1 && intGrid[2][1] == 1 && winnerPlus == false){
     win = true;
     greenWin = true;
+    winnerPlus = true;
+
+    if(winnerPlus == true){
+    greenWincounter+=1;
+    gridFull = 9;
     }
-    if (intGrid[0][2] == 1 && intGrid [1][2] == 1 && intGrid[2][2] == 1){
+    }
+    if (intGrid[0][2] == 1 && intGrid [1][2] == 1 && intGrid[2][2] == 1 && winnerPlus == false){
     win = true;
     greenWin = true;
+    winnerPlus = true;
+
+    if(winnerPlus == true){
+    greenWincounter+=1;
+    gridFull = 9;
+    }
     }
   }
+  /**
+   * Checks if green has 3 in a diagonal and sets win condition
+   */
   public void greenDiags(){
-    if (intGrid[0][0] == 1 && intGrid [1][1] == 1 && intGrid[2][2] == 1){
+    if (intGrid[0][0] == 1 && intGrid [1][1] == 1 && intGrid[2][2] == 1 && winnerPlus == false){
       win = true;
       greenWin = true;
+      winnerPlus = true;
+
+      if(winnerPlus == true){
+      greenWincounter+=1;
+      gridFull = 9;
       }
-      if (intGrid[0][2] == 1 && intGrid [1][1] == 1 && intGrid[2][0] == 1){
+      }
+      if (intGrid[0][2] == 1 && intGrid [1][1] == 1 && intGrid[2][0] == 1 && winnerPlus == false){
       win = true;
       greenWin = true;
+      winnerPlus = true;
+
+      if(winnerPlus == true){
+      greenWincounter+=1;
+      gridFull = 9;
+      }
       }
   
   }
+  /**
+   * Checks if blue has 3 in a column and sets win condition
+   */
   public void blooCols(){
-    if (intGrid[0][0] == 2 && intGrid[0][1] == 2 && intGrid[0][2] == 2) {
+    if (intGrid[0][0] == 2 && intGrid[0][1] == 2 && intGrid[0][2] == 2 && winnerPlus == false) {
       win = true;
       blooWin = true; 
+      winnerPlus = true;
+
+      if(winnerPlus == true){
+      blooWincounter+=1;
+      gridFull = 9;
+      }
   }
-  if (intGrid[1][0] == 2 && intGrid [1][1] == 2 && intGrid[1][2] == 2){
+  
+  if (intGrid[1][0] == 2 && intGrid [1][1] == 2 && intGrid[1][2] == 2 && winnerPlus == false){
     win = true;
     blooWin = true;
+    winnerPlus = true;
+
+      if(winnerPlus == true){
+      blooWincounter+=1;
+      gridFull = 9;
+      }
   }
-  if (intGrid[2][0] == 2 && intGrid [2][1] == 2 && intGrid[2][2] == 2){
+  if (intGrid[2][0] == 2 && intGrid [2][1] == 2 && intGrid[2][2] == 2 && winnerPlus == false){
     win = true;
     blooWin = true;
+    winnerPlus = true;
+
+      if(winnerPlus == true){
+      blooWincounter+=1;
+      gridFull = 9;
+      }
   }
 
   }
+  /**
+   * Checks if blue has 3 in a row and sets win condition
+   */
   public void blooRows(){
-    if (intGrid[0][0] == 2 && intGrid[1][0] == 2 && intGrid[2][0] == 2) {
+    if (intGrid[0][0] == 2 && intGrid[1][0] == 2 && intGrid[2][0] == 2 && winnerPlus == false) {
       win = true;
       blooWin = true; 
+      winnerPlus = true;
+
+      if(winnerPlus == true){
+      blooWincounter+=1;
+      gridFull = 9;
+      }
+
     }
-    if (intGrid[0][1] == 2 && intGrid [1][1] == 2 && intGrid[2][1] == 2){
+    if (intGrid[0][1] == 2 && intGrid [1][1] == 2 && intGrid[2][1] == 2 && winnerPlus == false){
     win = true;
     blooWin = true;
-    }
-    if (intGrid[0][2] == 2 && intGrid [1][2] == 2 && intGrid[2][2] == 2){
-    win = true;
-    blooWin = true;
-    }
-  }
-  public void blooDiags(){
-    if (intGrid[0][0] == 2 && intGrid [1][1] == 2 && intGrid[2][2] == 2){
-      win = true;
-      blooWin = true;
-      
-      }
-      if (intGrid[0][2] == 2 && intGrid [1][1] == 2 && intGrid[2][0] == 2){
-      win = true;
-      blooWin = true;
-      }
+
+    winnerPlus = true;
+
+    if(winnerPlus == true){
+    blooWincounter+=1;
+    gridFull = 9;
     }
 
-    public void Full(){
-      if (intGrid[0][0]==2 || intGrid [0][0]== 1){
-        isFull = true;
+    }
+    if (intGrid[0][2] == 2 && intGrid [1][2] == 2 && intGrid[2][2] == 2 && winnerPlus == false){
+    win = true;
+    blooWin = true;
+
+    winnerPlus = true;
+
+    if(winnerPlus == true){
+    blooWincounter+=1;
+    gridFull = 9;
+    }
+
+    }
+  }
+  /**
+   * Checks if blue has 3 in a diagonal and sets win condition
+   */
+  public void blooDiags(){
+    if (intGrid[0][0] == 2 && intGrid [1][1] == 2 && intGrid[2][2] == 2 && winnerPlus == false){
+      win = true;
+      blooWin = true;
+      winnerPlus = true;
+
+        if(winnerPlus == true){
+        blooWincounter+=1;
+        gridFull = 9;
+        }
       }
-      if (intGrid[1][0]==2 || intGrid [1][0]== 1){
-        isFull = true;
-      }
-      if (intGrid[2][0]==2 || intGrid [2][0]== 1){
-        isFull = true;
-      }
-      if (intGrid[0][1]==2 || intGrid [0][1]== 1){
-        isFull = true;
+      if (intGrid[0][2] == 2 && intGrid [1][1] == 2 && intGrid[2][0] == 2 && winnerPlus == false){
+      win = true;
+      blooWin = true;
+      winnerPlus = true;
+
+        if(winnerPlus == true){
+        blooWincounter+=1;
+        gridFull = 9;
+        }
       }
     }
+    /**
+  * Checks for when the mouse is pressed, fill a square with either Blue or green depending on whose turn it is. 
+  */
+
   public void mousePressed(){
     
     
-      if(turn == false && mouseButton == LEFT && win == false && gridFull <= 9 && intGrid [mouseY/200][mouseX/200] == 0){
+      if(turn == false && mouseButton == LEFT && win == false && gridFull <= 9 && intGrid [mouseY/200][mouseX/200] == 0 && blooWin == false && greenWin == false){
         intGrid [mouseY/200][mouseX/200] = 2;
         gridFull++;
         turn = true;
         
       }
-      else if (turn == true && mouseButton == LEFT&& win == false && gridFull <= 9 && intGrid [mouseY/200][mouseX/200] == 0){
+      else if (turn == true && mouseButton == LEFT&& win == false && gridFull <= 9 && intGrid [mouseY/200][mouseX/200] == 0 && blooWin == false && greenWin == false){
         intGrid [mouseY/200][mouseX/200] = 1; 
         gridFull++;
         turn = false;
@@ -295,23 +415,12 @@ public class Sketch2 extends PApplet {
     blooCols();
     blooRows();
     blooDiags();
-    blooWinCuter();
-    greenWinCuter();
-
-    Full();
    
 }
   
-  public void blooWinCuter(){
- if (win == true && blooWin == true){
-  blooWincounter+=1;
- }
-  }
-public void greenWinCuter(){
-  if (win == true && greenWin == true){
-    greenWincounter+=1;
-  }
-}
+/**
+  * Win Counter to detect who has won and increase the counter on screen by 1 for the respective player. 
+  */
   public void winCounter(){
 
 
@@ -323,7 +432,7 @@ public void greenWinCuter(){
     fill(0,0,255);
     text(blooWincounter,240,725);
 
-
+    
     if(blooWin == true){
       
       win = true;
@@ -358,7 +467,16 @@ public void greenWinCuter(){
 
       }
     }
+  
+    if (win == false && gridFull >= 9){
+      textSize(70);
+          fill(0,255,255);
+          text(tie,0,670);
+        
+      }
+    
 
   }
+
 
 }
